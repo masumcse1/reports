1a7df1e20d5b6bb45954db01bac9c60742cef20e
Md. Zohir Raihan <raihanzohir@gmail.com>
2024-10-18 23:45:08 +0600

    Server side implementation for 3 components (bookingDotComId, bookingDotComUrl, eHotelId)

--- src/main/java/com/property/report/service/property/PropertyStorageServiceImpl.java
+++ src/main/java/com/property/report/service/property/PropertyStorageServiceImpl.java
@@ -1,6 +1,5 @@
 package com.property.report.service.property;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.property.report.common.dto.AccessTokenSupplier;
@@ -19,7 +18,6 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -56,66 +54,71 @@ public class PropertyStorageServiceImpl implements PropertyStorageService {
                 country = new Country(handleCountryNullValue());
             }
 
-            AccessTokenSupplier tokenFromSuppliers = tokenService.getTokenFromSuppliers();
-            String token = tokenFromSuppliers.getAccessToken();
-
-            RestTemplate template = new RestTemplate();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.set("Authorization", "Bearer " + token);
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-
-            String apiUrl = supplierUrl
-                    + "properties/identifier/" + data.getId();
-
-            String bookingDotComId = "";
-            String bookingDotComUrl = "";
-            String eHotelId = "";
-
-            try {
-                ResponseEntity<?> response = template.exchange (
-                        apiUrl,
-                        HttpMethod.GET,
-                        entity,
-                        Map.class);
-
-                System.out.println("response.getStatusCode() = " + response.getStatusCode());
-                System.out.println("response = " + response);
-                System.out.println("response.getBody() = " + response.getBody());
-
-                // Ensure the response body is not null
-                if (response.getBody() != null) {
-                    Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-
-                    // Validate if 'result' exists and is of Map type
-                    List<PropertyIdentifier> propertyList = new ObjectMapper()
-                            .convertValue(responseBody.get("result"), new TypeReference<List<PropertyIdentifier>>() {});
-
-                    for (int i = 0; i < propertyList.size(); i++) {
-                        PropertyIdentifier propertyIdentifier = propertyList.get(i);
-
-
-                        if("Booking.com".equals(propertyIdentifier.getSource().getName())) {
-                            bookingDotComId = propertyIdentifier.getIdentifier();
-                            bookingDotComUrl = propertyIdentifier.getUrl();
-                        } else if("eHotel".equals(propertyIdentifier.getSource().getName())) {
-                            eHotelId = propertyIdentifier.getIdentifier();
-                        }
-
-                    }
-                } else {
-                    throw new IllegalStateException("API response body is null.");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            Result result = getPropertyIdentifierValues(data);
 
 //            Property property = new Property(data, data.getOnlinePresence(), country);
-            Property property = new Property(data, data.getOnlinePresence(), country, bookingDotComId, bookingDotComUrl, eHotelId);
+            Property property = new Property(data, data.getOnlinePresence(), country, result.bookingDotComId(), result.bookingDotComUrl(), result.eHotelId());
             propertyRepository.save(property);
         });
 
         log.info("---data save done for this page...");
+    }
+
+    private Result getPropertyIdentifierValues(PropertyDto data) {
+        AccessTokenSupplier tokenFromSuppliers = tokenService.getTokenFromSuppliers();
+        String token = tokenFromSuppliers.getAccessToken();
+
+        RestTemplate template = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        String apiUrl = supplierUrl
+                + "properties/identifier/" + data.getId();
+
+        String bookingDotComId = "";
+        String bookingDotComUrl = "";
+        String eHotelId = "";
+
+        try {
+            ResponseEntity<?> response = template.exchange (
+                    apiUrl,
+                    HttpMethod.GET,
+                    entity,
+                    Map.class);
+
+            // Ensure the response body is not null
+            if (response.getBody() != null) {
+                Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
+
+                // Validate if 'result' exists and is of Map type
+                List<PropertyIdentifier> propertyIdentifierList = new ObjectMapper()
+                        .convertValue(responseBody.get("result"), new TypeReference<List<PropertyIdentifier>>() {});
+
+                for (int i = 0; i < propertyIdentifierList.size(); i++) {
+                    PropertyIdentifier propertyIdentifier = propertyIdentifierList.get(i);
+
+
+                    if("Booking.com".equals(propertyIdentifier.getSource().getName())) {
+                        bookingDotComId = propertyIdentifier.getIdentifier();
+                        bookingDotComUrl = propertyIdentifier.getUrl();
+                    } else if("eHotel".equals(propertyIdentifier.getSource().getName())) {
+                        eHotelId = propertyIdentifier.getIdentifier();
+                    }
+
+                }
+            } else {
+                throw new IllegalStateException("API response body is null.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Result result = new Result(bookingDotComId, bookingDotComUrl, eHotelId);
+        return result;
+    }
+
+    private record Result(String bookingDotComId, String bookingDotComUrl, String eHotelId) {
     }
 
     public CountryDto handleCountryNullValue(){
